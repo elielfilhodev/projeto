f831e57614280de854074a0f7a24fa47b2baf9f3
elielfilho <elielfilho9@outlook.com>
2025-01-02 12:35:25 -0300

    playstation site

---
+++ script.js
@@ -0,0 +1,9 @@
+const sr = ScrollReveal({
+    distance: '65px',
+    duration: 2600,
+    delay: 450,
+    reset: true
+});
+
+sr.reveal('.conteudo',{delay:300, origin: 'bottom'});
+sr.reveal('.hero-img',{delay:550, origin: 'bottom'});
