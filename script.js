eaa70dd0baa515f5f38823e6b566a86a88737230
elielfilho <elielfilho9@outlook.com>
2025-01-02 22:09:24 -0300

    alteração em partes

--- script.js
+++ script.js
@@ -5,5 +5,29 @@ const sr = ScrollReveal({
     reset: true
 });
 
+let navbar = document.querySelector('.navbar');
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header .navbar a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute;
+
+        if(top >= offset && top < offset + height){
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header .navbar a [href*=' + id + ']').classList.add('active')
+            })
+        }
+    })
+}
+
+
 sr.reveal('.conteudo',{delay:300, origin: 'bottom'});
 sr.reveal('.hero-img',{delay:550, origin: 'bottom'});
+sr.reveal('.btn-1',{delay:700, origin: 'bottom'});
+sr.reveal('.btn-2',{delay:1500, origin: 'bottom'});
+sr.reveal('.btn-3',{delay:2200, origin: 'bottom'});
